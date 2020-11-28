04e04c774b863ebc61cdefd7cfc8158179c5df22
subash2617 <74961842+subash2617@users.noreply.github.com>
2020-11-28 09:46:15 +0530

    simple program to monitor the Body Temp

--- Task 5B.py
+++ Task 5B.py
@@ -2,14 +2,11 @@
 #Create a function covid( )#
 # it should accept patient name & Body temperature#
 # default the body temperature should be 98 degree#
-while True :
-    user_name = input("Enter the Name :")
-    user_input = input("Enter Body TEMP :")
-    if (int(user_input) < 98 or int(user_input) > 98):
-        is_valid = False
-        print("COVID19 ALERT")
-        break
-    else:
-        is_valid = True
-        print("Temp is NORMAL")
-        break
+def COVID19():
+    print("COVID19 ALERT")
+user_name = input("Enter the Name :")
+user_input = input("Enter Body TEMP :")
+if (int(user_input) > 98):
+    COVID19()
+else:
+    print("Temp is NORMAL")
